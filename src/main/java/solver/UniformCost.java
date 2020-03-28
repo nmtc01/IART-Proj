8ffde405f72b500735a72452a3c8441832ee9a64
Nuno <up201706162@fe.up.pt>
2020-03-28 17:58:33 +0000

    Uniform Cost needing test

--- src/main/java/solver/UniformCost.java
+++ src/main/java/solver/UniformCost.java
@@ -1,6 +1,6 @@
 package solver;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class UniformCost extends Solver {
 
@@ -8,8 +8,25 @@ public class UniformCost extends Solver {
         super(initboard);
     }
 
-    /*public solver.ExpansionTree.Node<ArrayList<ArrayList<Integer>>> perform() {
+    public ExpansionTree.Node<ArrayList<ArrayList<Integer>>> perform() {
+        PriorityQueue<ExpansionTree.Node<ArrayList<ArrayList<Integer>>>> pQueue = new PriorityQueue<ExpansionTree.Node<ArrayList<ArrayList<Integer>>>>(16, new NodeCostComparator());
+        ExpansionTree.Node<ArrayList<ArrayList<Integer>>> s = getCurrentState();
 
-    }*/
+        pQueue.add(s);
 
+        while(!pQueue.isEmpty()) {
+            ExpansionTree.Node<ArrayList<ArrayList<Integer>>> v = pQueue.remove();
+            this.setCurrentState(v);
+            this.expand_state();
+
+            for (int i = 0; i < v.getChildren().size(); i++) {
+                ExpansionTree.Node<ArrayList<ArrayList<Integer>>> w = v.getChildren().get(i);
+                if (isEnd(w))
+                    return w;
+                pQueue.add(w);
+            }
+        }
+
+        return getStates().getRoot();
+    }
 }
