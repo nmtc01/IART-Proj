3fd8c7957734914d2cc460b07cbeb51cd53200f1
Nuno <up201706162@fe.up.pt>
2020-04-03 11:48:51 +0100

    Added count of visited nodes on uniform cost

--- src/main/java/solver/UniformCost.java
+++ src/main/java/solver/UniformCost.java
@@ -4,15 +4,20 @@ import java.util.*;
 
 public class UniformCost extends Solver {
 
+    private int visitedNodes;
+
     public UniformCost(ArrayList<ArrayList<Integer>> initboard){
         super(initboard);
+        visitedNodes = 0;
     }
 
     public ExpansionTree.Node<ArrayList<ArrayList<Integer>>> perform() {
+        System.out.println("Number of visited Nodes:");
         PriorityQueue<ExpansionTree.Node<ArrayList<ArrayList<Integer>>>> pQueue = new PriorityQueue<ExpansionTree.Node<ArrayList<ArrayList<Integer>>>>(16, new NodeCostComparator());
         ExpansionTree.Node<ArrayList<ArrayList<Integer>>> s = getCurrentState();
 
         pQueue.add(s);
+        visitedNodes++;
 
         while(!pQueue.isEmpty()) {
             ExpansionTree.Node<ArrayList<ArrayList<Integer>>> v = pQueue.remove();
@@ -21,9 +26,12 @@ public class UniformCost extends Solver {
 
             for (int i = 0; i < v.getChildren().size(); i++) {
                 ExpansionTree.Node<ArrayList<ArrayList<Integer>>> w = v.getChildren().get(i);
-                if (isEnd(w))
+                if (isEnd(w)) {
+                    System.out.println("\nTotal number of visited Nodes: " + visitedNodes);
                     return w;
+                }
                 pQueue.add(w);
+                visitedNodes++;
             }
         }
 
